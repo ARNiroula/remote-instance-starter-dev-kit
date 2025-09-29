50ce0ef8cf32a6347d0b78b2f49a8675db598701
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-09-29 00:51:44 -0400

    Modify lazygit installation for debian ver < 13

--- install-prereq.sh
+++ install-prereq.sh
@@ -4,8 +4,12 @@
 sudo apt update
 sudo apt install tmux
 
-# Installing lazygit
-sudo apt install lazygit
+# Installing lazygit (For Ubuntu 25.04 and older)
+# sudo apt install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 # Installing Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
@@ -24,3 +28,5 @@ sudo apt install fzf
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 # git clone https://github.com/ARNiroula/neovim_config ~/.config/nvim
+
+source ~/.bashrc
